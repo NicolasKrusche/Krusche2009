ad93f1dc165b146129ffaebab5849840b0f5f1a1
NicolasKrusche <144341598+NicolasKrusche@users.noreply.github.com>
2023-09-20 19:42:38 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
-if (false) {
-    console.log("false");
-} else {
-    console.log("true");
-    console.log("true wird geschrieben obwohl false abgefragt wurde!");
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+document.querySelector("h1").onmouseover = event => {
+    let iterations = 0;
+
+    const interval = setInterval(() => {
+        event.target.innerText = event.target.innerText.split("").map((letter, index) => {
+            if(index < iterations) {
+                return event.target.dataset.value[index];
+            }
+            
+            return letters[Math.floor(Math.random() * 26)]}).join("");
+
+        if (iterations >= event.target.dataset.value.length) clearInterval(interval);
+
+        iterations += 1 / 3;
+
+    }, 20);
 }
+
+
+
+
+const letters2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+document.querySelector("h2").onmouseover = event => {
+    let iterations2 = 0;
+
+    const interval2 = setInterval(() => {
+        event.target.innerText = event.target.innerText.split("").map((letter, index) => {
+            if(index < iterations2) {
+                return event.target.dataset.value[index];
+            }
+            
+            return letters2[Math.floor(Math.random() * 26)]}).join("");
+
+        if (iterations2 >= event.target.dataset.value.length) clearInterval(interval2);
+
+        iterations2 += 1 / 3;
+
+    }, 20);
+}
+
