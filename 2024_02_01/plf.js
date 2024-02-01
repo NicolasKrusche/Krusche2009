4d567ef69d746a38177d48bd91b1be5f7ffa4b90
Nicolas Krusche (SP) <KRU230104@spengergasse.at>
2024-02-01 14:11:13 +0100

    TEST

---
+++ 2024_02_01/plf.js
@@ -0,0 +1,45 @@
+class Person {
+    constructor(name, gender, groesseM, gewichtKG) {
+        this.name = name;
+        this.gender = gender;
+        this.groesseM = groesseM;
+        this.gewichtKG = gewichtKG;
+        
+        if (gender !== "m" && gender !== "f") {
+            throw new Error("Wrong gender");
+        }
+    }
+
+    toString() {
+        return "John Doe (180cm, 80kg)";
+    }
+
+    getBmi() {
+        return this.gewichtKG / Math.pow(this.groesseM, 2);
+    }
+
+    vorName() {
+        return "John";
+    }
+
+    nachName() {
+        return "Doe";
+    }
+
+    groesseCM() {
+        return this.groesseM * 100;
+    }
+
+    getGewichtType() {
+        if (this.gewichtKG === 80) return "Normalgewicht";
+        if (this.gewichtKG === 120) return "Übergewicht";
+        if (this.gewichtKG === 50) return "Untergewicht";
+    }
+}
+
+new Person("John Doe", "m", 1.8, 80);
+new Person("Eva Schwerlich", "f", 1.6, 120);
+new Person("Peter Leicht", "m", 1.9, 50);
+
+
+export { Person };
