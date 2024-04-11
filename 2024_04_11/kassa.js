d0ead1c93f1f70da8154abdb5355dbbd6f47ba72
Nicolas Krusche (SP) <KRU230104@spengergasse.at>
2024-04-11 14:14:03 +0200

    ok

---
+++ 2024_04_11/kassa.js
@@ -0,0 +1,40 @@
+class Kassa {
+    #geldlade;
+    #gescannteArtikel;
+
+    constructor(geldlade){
+        this.#geldlade = geldlade;
+        this.#gescannteArtikel = []
+    }
+
+    scannen(artikel) {
+        this.#gescannteArtikel.push(artikel)
+    }
+
+
+    get betrag() {
+        let betrag = 0;
+        this.#gescannteArtikel.forEach(a => betrag + a.preis)
+        return betrag
+    }
+}
+
+
+class Artikel {
+
+    constructor(name, preis){
+        this.name = name;
+        this.preis = preis;
+    }
+
+}
+
+
+const produkt = new Artikel('Cola', 2)
+const produkt2 = new Artikel('Apfel', 1)
+const kassa = new Kassa()
+
+kassa.scannen(produkt)
+kassa.scannen(produkt2)
+
+console.log(`Offen sind: ${kassa.betrag}`)
