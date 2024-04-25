b528be0e5262d5964cafe625376da66774c4c508
Nicolas Krusche (SP) <KRU230104@spengergasse.at>
2024-04-25 13:35:51 +0200

    update kassa

--- 2024_04_11/kassa.js
+++ 2024_04_11/kassa.js
@@ -1,40 +1,53 @@
 class Kassa {
-    #geldlade;
-    #gescannteArtikel;
+    
+    #geldlade
 
-    constructor(geldlade){
-        this.#geldlade = geldlade;
-        this.#gescannteArtikel = []
+    constructor() {
+        this.#geldlade = 1000;
     }
 
     scannen(artikel) {
-        this.#gescannteArtikel.push(artikel)
+
+        console.log(`Gescannter Artikel: ${artikel.name} -> Preis: ${artikel.preis} €`);
+        
+        return artikel.preis;
     }
 
+    bezahlen(betrag, preis) {
 
-    get betrag() {
-        let betrag = 0;
-        this.#gescannteArtikel.forEach(a => betrag += a.preis)
-        return betrag
+        if (betrag < preis) {
+            throw new Error('Nicht genug Geld bezahlt!');
+        }
+
+        else { 
+            this.#geldlade = this.#geldlade + preis;
+            const rueckgeld = betrag - preis;
+            console.log(`Bezahlt: ${preis.toFixed(2)} € - Rückgeld: ${rueckgeld.toFixed(2)} €`);
+            return rueckgeld;
+        }
     }
+
+    toString() {
+        return `Heute ${(this.#geldlade - 10).toFixed(2)} € eingenommen.`;
+    }
+    
 }
-
 
 class Artikel {
-
-    constructor(name, preis){
-        this.name = name;
-        this.preis = preis;
+    constructor(name, preis) {
+      this.name = name;
+      this.preis = preis;
     }
+  }
 
-}
+
+const artikel = {'name': 'Brot', 'preis': 1.89};
 
 
-const produkt = new Artikel('Cola', 2)
-const produkt2 = new Artikel('Apfel', 1)
-const kassa = new Kassa()
+const kassa = new Kassa();
 
-kassa.scannen(produkt)
-kassa.scannen(produkt2)
+const preis = kassa.scannen(artikel);
 
-console.log(`Offen sind: ${kassa.betrag}`)
+const rueckgeld = kassa.bezahlen(2.50, preis);
+
+console.log(kassa.toString());
